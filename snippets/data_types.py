8f3ffc5825ecefb29b2486d5014c386b93977a41
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-18 14:34:18 +0545

    Create data_types.py

---
+++ snippets/data_types.py
@@ -0,0 +1,32 @@
+x = "Hello World"
+#str	
+x = 20
+#int	
+x = 20.5	
+#float	
+x = True	
+#bool	
+#more...
+x = 1j	
+#complex	
+x =["apple", "banana", "cherry"]	
+#list	
+x = ("apple", "banana", "cherry")	
+#tuple	
+x = range(6)	
+#range	
+x = {"name" : "John", "age" : 36}	
+#dict	
+x = {"apple", "banana", "cherry"}	
+#set	
+x = frozenset({"apple", "banana", "cherry"})	
+#frozenset	
+
+x = b"Hello"	
+#bytes	
+x = bytearray(5)	
+#bytearray	
+x = memoryview(bytes(5))	
+#memoryview	
+x = None	
+#NoneType	
