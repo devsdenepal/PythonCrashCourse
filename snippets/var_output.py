9b6e8abf2727087f0db27f97e99dd0afa7cc1cd6
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-18 14:31:38 +0545

    Create var_output.py

---
+++ snippets/var_output.py
@@ -0,0 +1,15 @@
+akash_age =13
+suraj_age = 14
+aayush_age = 15
+line1 = "I love"
+line2 = "Python"
+line3 = "Very much"
+# printing multiple variable value on single command
+print(akash_age,suraj_age,aayush_age)
+# Output : 13 14 15
+print(line1, line2, line3)
+#output : I love Python Very much
+print(line1 + line2 + line3)
+#you can also use '+' operator to output multiple variable at once
+print(akash_age+suraj_age+aayush_age)
+#[output : 42] (!) You can also use '+' operator to perform addition 
