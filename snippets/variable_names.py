6d4a4631e566a56cee67fda938185f48ab06190d
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-18 14:23:15 +0545

    Update variable_names.py

--- snippets/variable_names.py
+++ snippets/variable_names.py
@@ -6,3 +6,13 @@ first_var = 1
 2myvar = "John"
 my-var = "John"
 my var = "John"
+# multiple value to multiple variable in one  assignment
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# one value to multiple variable in multiple assignmets
+akash_age = suraj_age = aayush_age = 14
+print(akash_age)
+print(suraj_age)
+print(aayush_age)
