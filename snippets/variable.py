8450b0df02c5c902e8bc6af32b50ad9f0b7b5ed2
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-18 14:14:29 +0545

    Update variable.py

--- snippets/variable.py
+++ snippets/variable.py
@@ -2,4 +2,12 @@ person = "John Doe"
 age = 21
 male = True
 female = False
+# output the value of 'person' variable
 print(person)
+# ------- Case sensitive
+a = 9
+A = 8
+print(a)
+# outputs 9
+print(A)
+#outputs 8
