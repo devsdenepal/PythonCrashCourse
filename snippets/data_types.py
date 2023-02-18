a661f141e2ef189da44d9664c0e25b4923a0c312
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-18 14:36:47 +0545

    Update data_types.py

--- snippets/data_types.py
+++ snippets/data_types.py
@@ -1,32 +1,47 @@
 x = "Hello World"
-#str	
+print(type(x))
+#output str	
 x = 20
+print(type(x))
 #int	
 x = 20.5	
+print(type(x))
 #float	
 x = True	
+print(type(x))
 #bool	
 #more...
 x = 1j	
+print(type(x))
 #complex	
 x =["apple", "banana", "cherry"]	
+print(type(x))
 #list	
 x = ("apple", "banana", "cherry")	
+print(type(x))
 #tuple	
 x = range(6)	
+print(type(x))
 #range	
 x = {"name" : "John", "age" : 36}	
+print(type(x))
 #dict	
 x = {"apple", "banana", "cherry"}	
+print(type(x))
 #set	
 x = frozenset({"apple", "banana", "cherry"})	
+print(type(x))
 #frozenset	
 
 x = b"Hello"	
+print(type(x))
 #bytes	
-x = bytearray(5)	
+x = bytearray(5)
+print(type(x))
 #bytearray	
-x = memoryview(bytes(5))	
+x = memoryview(bytes(5))
+print(type(x))
 #memoryview	
 x = None	
+print(type(x))
 #NoneType	
